4f00480ed35324a37f35c3fa10e0d3a0c251788b
Cormac Relf <web@cormacrelf.net>
2019-02-16 19:13:11 +1100

    apply to all text fields

--- pandoc-preproc/src/Lib.hs
+++ pandoc-preproc/src/Lib.hs
@@ -1,12 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( someFunc
+    ( parseTextFields
     ) where
 
 import Text.Pandoc.Definition
 import Text.Pandoc.Shared (blocksToInlines)
 import Text.Pandoc.Class (runPure)
+import qualified Data.Set as Set
+import qualified Data.Vector as V
 import qualified Text.Pandoc.Readers.Markdown as PM
 import qualified Text.Pandoc.JSON as PJ
 import qualified Data.Text as T
@@ -23,31 +25,90 @@ import qualified Data.Aeson as A
 import Control.Lens
 import Data.Aeson.Lens
 
-someFunc :: IO ()
-someFunc = readJSON
+textFields :: Set.Set T.Text
+textFields = Set.fromList [
+      -- these should probably be parsed as Blocks
+      "annote",
+      "note",
+      "abstract",
 
--- zoom :: IO ()
--- zoom = do
---     result <- runIO $ do
---         doc <- readMarkdown def (T.pack "[testing](https://example.com)")
---         writeJSON
+      -- these must be verbatim, so don't parse them
+      -- "URL",
+      -- "PMCID",
+      -- "PMID",
+      -- "ISBN",
+      -- "ISSN",
+      -- "DOI",
 
-readJSON :: IO ()
-readJSON = do
-    json <- BL.readFile "lib.json"
+      "archive",
+      "archive-location",
+      "archive-place",
+      "authority",
+      "call-number",
+      "citation-label",
+      "collection-title",
+      "container-title",
+      "container-title-short",
+      "dimensions",
+      "event",
+      "event-place",
+      "genre",
+      "keyword",
+      "medium",
+      "original-publisher",
+      "original-publisher-place",
+      "original-title",
+      "publisher",
+      "publisher-place",
+      "references",
+      "reviewed-title",
+      "scale",
+      "section",
+      "source",
+      "status",
+      "title",
+      "title-short",
+      "version",
+
+      -- virtual-only variables
+      "year-suffix",
+
+      -- CSL-M
+      "available-date",
+      "volume-title",
+      "committee",
+      "document-name",
+      "gazette-flag"
+
+      -- CSL-M verbatim
+      -- "language",
+      -- "jurisdiction",
+
+      -- virtual-only variables
+      -- "hereinafter",
+    ]
+
+parseTextFields :: IO ()
+parseTextFields = do
+    json <- BL.getContents
     BL.putStr $
-        json & values . key "title" %~ parseInlines
-
+        json & values
+             . _Object
+             . traverseTextFields
+             %~ parseInlines
 
     where
+        traverseTextFields = itraversed . indices (`Set.member` textFields)
 
-        -- unwraps errors and blows up if we can't parse, etc
+        -- we'll just silently replace unparsable stuff with []
+        emp = A.Array V.empty
+
         parseInlines :: Value -> Value
-        parseInlines (A.String x) = fromRight (A.String "") result
+        parseInlines (A.String x) = fromRight emp result
             where
                 result = runPure $ do
                     doc <- PM.readMarkdown def x
                     let inlines = case doc of
                                     Pandoc meta blocks -> blocksToInlines blocks
                     return $ toJSON inlines
-        parseInlines _ = (A.String "")
+        parseInlines _ = emp
