553669f246b6fdbb2933154c1c5b01b2a5a4eab5
Cormac Relf <web@cormacrelf.net>
2019-02-16 23:00:12 +1100

    implement block parsing for annote etc, commented for later

--- pandoc-preproc/src/Lib.hs
+++ pandoc-preproc/src/Lib.hs
@@ -27,6 +27,11 @@ import qualified Data.Aeson as A
 import Control.Lens
 import Data.Aeson.Lens
 
+blockFields :: Set.Set T.Text
+blockFields = Set.fromList [ "annote"
+                           , "note"
+                           , "abstract" ]
+
 textFields :: Set.Set T.Text
 textFields = Set.fromList [
       -- these should probably be parsed as Blocks
@@ -95,22 +100,34 @@ parseTextFields = do
     BL.putStr $
         json & values
              . _Object
-             . traverseTextFields
-             %~ parseInlines
+             . itraversed %@~ allFields
 
     where
-        traverseTextFields = itraversed . indices (`Set.member` textFields)
+        allFields field value
+            | field `Set.member` textFields = parseInlines value
+            -- | field `Set.member` blockFields = parseBlocks value
+            | otherwise                     = value
 
         -- we'll just silently replace unparsable stuff with []
         emp = A.Array V.empty
 
         parseInlines :: Value -> Value
         parseInlines (A.String txt) = fromRight emp $ runPure $ do
-            Pandoc _ blocks <- readMarkdown markdownOptions txt
+            Pandoc _ blocks <- readMarkdown (markdownOptions inlineExtensions) txt
             return $ toJSON $ blocksToInlines blocks
         parseInlines _ = emp
 
-        markdownExtensions = extensionsFromList
+        parseBlocks :: Value -> Value
+        parseBlocks (A.String txt) = fromRight emp $ runPure $ do
+            Pandoc _ blocks <- readMarkdown (markdownOptions blockExtensions) txt
+            return $ toJSON $ blocks
+        parseBlocks _ = emp
+
+        markdownOptions exts = def { readerExtensions = exts
+                                   , readerStandalone = False
+                                   , readerStripComments = True }
+
+        inlineExtensions = extensionsFromList
             [ Ext_raw_html
             , Ext_native_spans
             , Ext_native_divs
@@ -127,7 +144,10 @@ parseTextFields = do
             , Ext_smart
             ]
 
-        markdownOptions :: ReaderOptions
-        markdownOptions = def { readerExtensions = markdownExtensions
-                              , readerStandalone = False
-                              , readerStripComments = True }
+        blockExtensions
+            = disableExtension Ext_inline_notes
+            . disableExtension Ext_citations
+            . disableExtension Ext_footnotes
+            . disableExtension Ext_pandoc_title_block
+            . disableExtension Ext_yaml_metadata_block
+            $ pandocExtensions
