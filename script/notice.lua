351376c7581bbeb7663f74e53a24b3b3e859b810
Cormac Relf <web@cormacrelf.net>
2019-06-28 17:18:27 +1000

    MPL 2.0 license notices on Haskell/Lua source

--- script/notice.lua
+++ script/notice.lua
@@ -2,7 +2,5 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at http://mozilla.org/MPL/2.0/.
 --
--- Copyright © 2018 Corporation for Digital Scholarship
+-- Copyright © COPYRIGHT_YEAR Corporation for Digital Scholarship
 
-import Distribution.Simple
-main = defaultMain
