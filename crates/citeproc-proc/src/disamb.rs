9cc0866198172e59b4bb2be6e6338b809927c35e
Cormac Relf <web@cormacrelf.net>
2019-07-31 05:00:03 +1000

    construct NFAs and convert them to DFAs (for disambiguation matching)
    - the idea is to transform a Reference into a DFA and then run it against
      the tokens emitted while processing a Cite.
    - a further extension is to unify & minify ALL the DFAs of all the references
      and use them in a single FST that stores reference ids.
    - may be difficult to remove parts of the FST though (if a reference gets changed).
    - that would be fn matching_refs(processed_cite) -> Vec<ReferenceId>;

--- crates/citeproc-proc/src/disamb.rs
+++ crates/citeproc-proc/src/disamb.rs
@@ -118,3 +118,320 @@ impl AddDisambTokens for Date {
         }
     }
 }
+
+use std::collections::HashMap;
+use std::collections::BTreeSet;
+use csl::style::Formatting;
+use citeproc_io::output::OutputFormat;
+
+use petgraph::graph::{Graph, NodeIndex};
+
+// // NFA, but only one epsilon transition per node.
+// // Two if branches in a row = A -ε-> B(if1) -ε-> C(if2)
+#[derive(Clone, PartialEq, Eq, Hash)]
+struct NfaToken(String, Formatting);
+
+use std::fmt::{Debug, Formatter};
+
+impl Debug for NfaToken {
+    fn fmt(&self, f: &mut Formatter) -> std::fmt::Result {
+        if self.1 == Formatting::default() {
+            write!(f, "{}", self.0)
+        } else {
+            write!(f, "NfaToken({:?}, {:?})", self.0, self.1)
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+enum NfaEdge {
+    Epsilon,
+    Token(NfaToken),
+}
+
+impl<'a> From<&'a str> for NfaToken {
+    fn from(s: &'a str) -> Self {
+        NfaToken(s.to_owned(), Formatting::default())
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum NfaNode {
+    Normal,
+    Accepting,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+enum DfaNode {
+    Normal,
+    Accepting,
+}
+
+type NfaGraph = Graph<NfaNode, NfaEdge>;
+type DfaGraph = Graph<DfaNode, NfaToken>;
+
+fn epsilon_closure(nfa: &NfaGraph, closure: &mut BTreeSet<NodeIndex>) {
+    let mut work: Vec<_> = closure.iter().cloned().collect();
+    while !work.is_empty() {
+        let s = work.pop().unwrap();
+        for node in nfa.neighbors(s) {
+            let is_epsilon = nfa.find_edge(s, node)
+                .and_then(|e| nfa.edge_weight(e))
+                .map(|e| e == &NfaEdge::Epsilon)
+                .unwrap_or(false);
+            if is_epsilon && !closure.contains(&node) {
+                work.push(node);
+                closure.insert(node);
+            }
+        }
+    }
+}
+
+#[derive(Debug)]
+struct Nfa {
+    graph: NfaGraph,
+    accepting: BTreeSet<NodeIndex>,
+    start: NodeIndex,
+}
+
+#[derive(Debug)]
+struct Dfa {
+    graph: DfaGraph,
+    // TODO: you can use the node data to store this
+    // accepting: BTreeSet<NodeIndex>,
+    start: NodeIndex,
+}
+
+// TODO: find the quotient automaton of the resulting DFA?
+fn to_dfa(nfa: &Nfa) -> Dfa {
+    let mut dfa = DfaGraph::new();
+
+    let mut work = Vec::new();
+    let mut start_set = BTreeSet::new();
+    start_set.insert(nfa.start);
+    epsilon_closure(&nfa.graph, &mut start_set);
+    let dfa_start_node = dfa.add_node(DfaNode::Normal);
+    for s in start_set.iter() {
+        if let Some(NfaNode::Accepting) = nfa.graph.node_weight(*s) {
+            let n = dfa.node_weight_mut(dfa_start_node).unwrap();
+            *n = DfaNode::Accepting;
+            break;
+        }
+    }
+    work.push((start_set.clone(), dfa_start_node));
+
+    let mut dfa_states = HashMap::new();
+    dfa_states.insert(start_set, dfa_start_node);
+
+    while !work.is_empty() {
+        let (dfa_state, current_node) = work.pop().unwrap();
+        let mut by_edge_weight = HashMap::<NfaToken, BTreeSet<NodeIndex>>::new();
+        for nfa_node in dfa_state {
+            for neigh in nfa.graph.neighbors(nfa_node) {
+                let weight = nfa.graph.find_edge(nfa_node, neigh)
+                    .and_then(|e| nfa.graph.edge_weight(e))
+                    .cloned()
+                    .unwrap();
+                if let NfaEdge::Token(t) = weight {
+                by_edge_weight.entry(t)
+                    .and_modify(|set| {
+                        set.insert(neigh);
+                    })
+                    .or_insert_with(|| {
+                        let mut set = BTreeSet::new();
+                        set.insert(neigh);
+                        set
+                    });
+                }
+            }
+        }
+        for (k, mut set) in by_edge_weight.drain() {
+            epsilon_closure(&nfa.graph, &mut set);
+            if !dfa_states.contains_key(&set)  {
+                let node = dfa.add_node(DfaNode::Normal);
+                for s in set.iter() {
+                    if let Some(NfaNode::Accepting) = nfa.graph.node_weight(*s) {
+                        let n = dfa.node_weight_mut(node).unwrap();
+                        *n = DfaNode::Accepting;
+                        break;
+                    }
+                }
+                dfa_states.insert(set.clone(), node);
+                dfa.add_edge(current_node, node, k);
+                work.push((set, node));
+            } else {
+                let &node = dfa_states.get(&set).unwrap();
+                dfa.add_edge(current_node, node, k);
+            }
+        }
+    }
+    Dfa {
+        graph: dfa,
+        start: dfa_start_node,
+    }
+}
+
+impl Dfa {
+    fn accepts(&self, tokens: &[NfaToken]) -> bool {
+        let mut cursor = self.start;
+        for token in tokens {
+            let mut found = false;
+            for neighbour in self.graph.neighbors(cursor) {
+                let weight = self.graph.find_edge(cursor, neighbour)
+                    .and_then(|e| self.graph.edge_weight(e))
+                    .map(|w| w == token);
+                if let Some(true) = weight {
+                    cursor = neighbour;
+                    found = true;
+                    break;
+                }
+            }
+            if !found {
+                return false;
+            }
+        }
+        self.graph.node_weight(cursor).unwrap() == &DfaNode::Accepting
+    }
+}
+
+#[test]
+fn nfa() {
+    let andy = NfaEdge::Token("Andy".into());
+    let reuben = NfaEdge::Token("Reuben".into());
+    let peters = NfaEdge::Token("Peters".into());
+    let comma = NfaEdge::Token(",".into());
+    let twenty = NfaEdge::Token("2017".into());
+
+    let mut nfa = {
+        let mut nfa = NfaGraph::new();
+        let initial = nfa.add_node(NfaNode::Normal);
+        let forwards1 = nfa.add_node(NfaNode::Normal);
+        let backwards1 = nfa.add_node(NfaNode::Normal);
+        let backwards2 = nfa.add_node(NfaNode::Normal);
+        let last_only = nfa.add_node(NfaNode::Normal);
+        let target = nfa.add_node(NfaNode::Normal);
+        let abc = nfa.add_node(NfaNode::Normal);
+        let acc = nfa.add_node(NfaNode::Accepting);
+        nfa.add_edge(initial, forwards1, reuben.clone());
+        nfa.add_edge(forwards1, target, peters.clone());
+        nfa.add_edge(initial, backwards1, peters.clone());
+        nfa.add_edge(backwards1, backwards2, comma.clone());
+        nfa.add_edge(backwards2, target, reuben.clone());
+        nfa.add_edge(initial, target, peters.clone());
+        nfa.add_edge(target, abc, comma.clone());
+        nfa.add_edge(abc, acc, twenty.clone());
+        let mut accepting = BTreeSet::new();
+        accepting.insert(acc);
+        Nfa {
+            graph: nfa,
+            accepting,
+            start: initial,
+        }
+    };
+
+    let mut nfa2 = {
+        let mut nfa = NfaGraph::new();
+        let initial = nfa.add_node(NfaNode::Normal);
+        let forwards1 = nfa.add_node(NfaNode::Normal);
+        let backwards1 = nfa.add_node(NfaNode::Normal);
+        let backwards2 = nfa.add_node(NfaNode::Normal);
+        let last_only = nfa.add_node(NfaNode::Normal);
+        let target = nfa.add_node(NfaNode::Normal);
+        let abc = nfa.add_node(NfaNode::Normal);
+        let acc = nfa.add_node(NfaNode::Accepting);
+        nfa.add_edge(initial, forwards1, andy.clone());
+        nfa.add_edge(forwards1, target, peters.clone());
+        nfa.add_edge(initial, backwards1, peters.clone());
+        nfa.add_edge(backwards1, backwards2, comma.clone());
+        nfa.add_edge(backwards2, target, andy.clone());
+        nfa.add_edge(initial, target, peters.clone());
+        nfa.add_edge(target, abc, comma.clone());
+        nfa.add_edge(abc, acc, twenty.clone());
+        let mut accepting = BTreeSet::new();
+        accepting.insert(acc);
+        Nfa {
+            graph: nfa,
+            accepting,
+            start: initial,
+        }
+    };
+
+    // You can't do this, because it will overwrite the `middle -ho-> target` edge above
+    // nfa.add_edge(middle, target, NfaEdge::Epsilon);
+
+    let dfa = to_dfa(&nfa);
+    let dfa2 = to_dfa(&nfa2);
+    println!("{:?}", dfa.start);
+    use petgraph::dot::Dot;
+    println!("{:?}", Dot::with_config(&dfa.graph, &[]));
+    println!("{:?}", Dot::with_config(&dfa2.graph, &[]));
+
+    assert!(dfa.accepts(&[ "Peters".into(), ",".into(), "2017".into() ]));
+    assert!(dfa2.accepts(&[ "Peters".into(), ",".into(), "2017".into() ]));
+
+    assert!(dfa.accepts(&[ "Reuben".into(), "Peters".into(), ",".into(), "2017".into() ]));
+    assert!(dfa.accepts(&[ "Peters".into(), ",".into(), "Reuben".into(), ",".into(), "2017".into() ]));
+    assert!(!dfa.accepts(&[ "Peters".into(), ",".into(), "Andy".into(), ",".into(), "2017".into() ]));
+    assert!(!dfa.accepts(&[ "Andy".into(), "Peters".into(), ",".into(), "2017".into() ]));
+
+    assert!(dfa2.accepts(&[ "Andy".into(), "Peters".into(), ",".into(), "2017".into() ]));
+    assert!(!dfa2.accepts(&[ "Peters".into(), ",".into(), "Reuben".into(), ",".into(), "2017".into() ]));
+    assert!(!dfa2.accepts(&[ "Reuben".into(), "Peters".into(), ",".into(), "2017".into() ]));
+
+}
+
+// struct DisambNfa {
+//     state: NfaIndex,
+//     edges: Vec<HashMap<Option<NfaToken>, NfaIndex>>,
+//     accepting: HashSet<NfaIndex>,
+// }
+
+// struct DisambiguationState<O: OutputFormat> {
+//     format_stack: Vec<Formatting>,
+//     format_current: Formatting,
+//     dfa: DisambNfa<O>,
+// }
+
+// impl DisambiguationState {
+//     /// if None, you should exit out of DFADisambiguate
+//     fn digest(&mut self, token: DisambToken) -> Option<()> {
+//         // ... match against internal DFA, using formatting from the stack.
+//     }
+//     fn push_fmt(&mut self, fmt: Formatting) {
+//         self.format_stack.push(fmt);
+//         self.coalesce_fmt();
+//     }
+//     fn pop_fmt(&mut self) {
+//         self.format_stack.pop();
+//         self.coalesce_fmt();
+//     }
+//     fn coalesce_fmt(&mut self) {
+//         self.format_current = Formatting::default();
+//         for fmt in self.format_stack.iter() {
+//             self.format_current.font_style = fmt.font_style;
+//             self.format_current.font_weight = fmt.font_weight;
+//             self.format_current.font_variant = fmt.font_variant;
+//             self.format_current.text_decoration = fmt.text_decoration;
+//             self.format_current.vertical_alignment = fmt.vertical_alignment;
+//         }
+//     }
+// }
+
+// trait DFADisambiguate {
+//     fn traverse(&self, state: &mut DisambiguationState);
+// }
+
+// impl DFADisambiguate for Vec<Node> {
+//     fn traverse(&self, state: &mut DisambiguationState) -> Option<()> {
+//         match self {
+//             Text(s) => for token in s.split(" ") { state.digest(token)?; },
+//             Fmt(f, ts) => {
+//                 state.push_fmt(f);
+//                 for token in ts {
+//                     state.digest(token)?;
+//                 }
+//                 state.pop_fmt();
+//             }
+//         }
+//     }
+// }
