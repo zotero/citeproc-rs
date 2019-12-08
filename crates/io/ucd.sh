59c5977d8b71784e66af6120590e102448895c13
Cormac Relf <web@cormacrelf.net>
2019-12-08 22:23:42 +1100

    Actually write out files in proc/ucd.sh

--- crates/io/ucd.sh
+++ crates/io/ucd.sh
@@ -1,9 +1,9 @@
 VERSION=12.1.0
 
-(mkdir -p /tmp/ucd && \
+mkdir -p /tmp/ucd && \
 cd /tmp/ucd && \
 curl -LO https://www.unicode.org/Public/zipped/$VERSION/UCD.zip && \
 unzip -o UCD.zip && \
 grep '<super>' UnicodeData.txt > Superscript.txt && \
-grep '<sub>' UnicodeData.txt > Subscript.txt)
+grep '<sub>' UnicodeData.txt > Subscript.txt
 
--- crates/proc/ucd.sh
+++ crates/proc/ucd.sh
@@ -1,10 +1,12 @@
 # cargo install --force ucd-generate
 VERSION=12.1.0
 
-(mkdir -p /tmp/ucd && \
+PROC=$(pwd)
+
+mkdir -p /tmp/ucd && \
 cd /tmp/ucd && \
 curl -LO https://www.unicode.org/Public/zipped/$VERSION/UCD.zip && \
 unzip -o UCD.zip && \
-mkdir -p src/input/unicode/ && \
-ucd-generate script /tmp/ucd --include Common,Latin,Cyrillic --trie-set > src/input/unicode/script.rs)
+mkdir -p $PROC/src/unicode && \
+ucd-generate script /tmp/ucd --include Common,Latin,Cyrillic --trie-set > $PROC/src/unicode/script.rs
 
