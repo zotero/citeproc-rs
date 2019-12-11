0d191c0d1685b667096f8a5bf4ce5c9dd0587d3d
Cormac Relf <web@cormacrelf.net>
2019-12-11 16:09:48 +1100

    implement some of TextCase in IngestOptions::plain

--- crates/io/src/lib.rs
+++ crates/io/src/lib.rs
@@ -51,12 +51,45 @@ pub struct IngestOptions {
     pub strip_periods: bool,
 }
 
+/// https://stackoverflow.com/a/38406885
+fn uppercase_first(s: &str) -> String {
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
+
+fn transform_first_char_of_word(s: &str, f: impl Fn(char) -> char) -> String {
+    // Naively capitalizes without a stopword filter
+    let chars = s.chars();
+    for c in words {
+    }
+    let mut c = s.chars();
+    match c.next() {
+        None => String::new(),
+        Some(f) => f.to_uppercase().collect::<String>() + c.as_str(),
+    }
+}
+
 impl IngestOptions {
     pub fn plain<'s>(&self, s: &'s str) -> Cow<'s, str> {
+        let mut cow = Cow::Borrowed(s);
         if self.replace_hyphens {
-            Cow::Owned(s.replace('-', "\u{2013}"))
-        } else {
-            Cow::Borrowed(s)
+            cow = Cow::Owned(cow.replace('-', "\u{2013}"));
+        }
+        if self.strip_periods {
+            cow = Cow::Owned(cow.replace('.', ""));
+        }
+        match self.text_case {
+            TextCase::None => cow,
+            TextCase::Lowercase => Cow::Owned(cow.to_lowercase()),
+            TextCase::Uppercase => Cow::Owned(cow.to_uppercase()),
+            TextCase::Uppercase => Cow::Owned(cow.to_uppercase()),
+            TextCase::CapitalizeFirst => Cow::Owned(uppercase_first(&cow)),
+            TextCase::CapitalizeAll => Cow::Owned(uppercase_first(&cow)),,
+            TextCase::Sentence,
+            TextCase::Title,
         }
     }
     pub fn default_with_quotes(quotes: LocalizedQuotes) -> Self {
