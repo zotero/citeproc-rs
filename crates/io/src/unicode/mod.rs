586b520f28c4917d8cb1ff695220d4213bc239b3
Cormac Relf <web@cormacrelf.net>
2020-11-02 01:43:32 +1100

    Move UCD to io crate for one-time-only latin cyrillic name detection

--- crates/io/src/unicode/mod.rs
+++ crates/io/src/unicode/mod.rs
@@ -1,2 +1,32 @@
+#[allow(clippy::all)]
 #[rustfmt::skip]
 pub(crate) mod sup_sub;
+
+#[allow(clippy::all)]
+#[rustfmt::skip]
+#[allow(dead_code)]
+mod latin_cyrillic;
+use self::latin_cyrillic::{COMMON, CYRILLIC, GREEK, LATIN};
+
+pub fn is_latin_cyrillic(s: &str) -> bool {
+    s.chars().all(|c| {
+        LATIN.contains_char(c)
+            || COMMON.contains_char(c)
+            || CYRILLIC.contains_char(c)
+            || GREEK.contains_char(c)
+    })
+}
+
+#[test]
+fn test_is_latin_cyrillic() {
+    assert!(is_latin_cyrillic(" @")); // Common only
+    assert!(is_latin_cyrillic("ÀÖ hello world")); // Latin only
+    assert!(is_latin_cyrillic("'҇ԯ")); // Cyrillic only
+    assert!(is_latin_cyrillic("ÀÖ '҇ԯ@ ")); // Latin and Cyrillic and Common
+    assert!(!is_latin_cyrillic("⺙⺛⻳")); // Han
+    assert!(!is_latin_cyrillic("⺙.⺛⻳")); // Han with common
+
+    // Extras
+    assert!(is_latin_cyrillic("ἀἕἘ")); // Greek
+    assert!(is_latin_cyrillic("Άγρας")) // Greek
+}
--- crates/io/ucd.sh
+++ crates/io/ucd.sh
@@ -1,9 +1,11 @@
+set -euxo pipefail
+
 VERSION=12.1.0
 
 mkdir -p /tmp/ucd && \
 cd /tmp/ucd && \
-curl -LO https://www.unicode.org/Public/zipped/$VERSION/UCD.zip && \
-unzip -o UCD.zip && \
-grep '<super>' UnicodeData.txt > Superscript.txt && \
-grep '<sub>' UnicodeData.txt > Subscript.txt
-
+curl -LO https://www.unicode.org/Public/zipped/$VERSION/UCD.zip || (echo failed to download $VERSION/UCD.zip && exit 1)
+unzip -o UCD.zip || (echo failed to unzip UCD.zip && exit 1)
+grep '<super>' UnicodeData.txt > Superscript.txt || (echo failed to write Superscript.txt && exit 1)
+grep '<sub>' UnicodeData.txt > Subscript.txt || (echo failed to write Subscript.txt && exit 1)
+ucd-generate script /tmp/ucd --include Common,Latin,Cyrillic,Greek --trie-set > "$UCD_RUST_OUT_DIR/latin_cyrillic.rs" || exit 1
