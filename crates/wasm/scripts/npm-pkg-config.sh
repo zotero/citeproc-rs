bbae24210fcfa09696c76b94dded67dd2dae4ac1
Cormac Relf <web@cormacrelf.net>
2020-12-04 02:12:39 +1100

    npm: Copy js snippets; --targets; fix commonjs output again
    So this build script is getting a bit involved, but that's
    the fragmented JS module ecosystem for you.

--- crates/wasm/scripts/npm-pkg-config.sh
+++ crates/wasm/scripts/npm-pkg-config.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Run this script from the crates/wasm folder.
+set -euo pipefail
+shopt -s nullglob
+shopt -s extglob
+shopt -s globstar
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+DIR="$( cd .. && pwd )"
 
 CLEAR='\033[0m'
 RED='\033[0;31m'
@@ -19,6 +25,7 @@ function usage() {
   echo "  --package-only                         Don't rebuild, just rewrite the package.json"
   echo "  --github-packages @scope/repo/pkg-name Configure for publishing to GitHub packages, @scope/pkg-name in repo @scope/repo"
   echo "  --features                             List of cargo features to enable (comma-sep)"
+  echo "  --targets                              List of npm targets to build (comma-sep, default all)"
   echo
   exit 1
 }
@@ -31,6 +38,7 @@ PACKAGE_ONLY=
 GITHUB_PACKAGES_DEF=
 CANARY_SHA=
 FEATURES=
+TARGETS=
 
 # parse params
 while [[ "$#" > 0 ]]; do case $1 in
@@ -42,6 +50,7 @@ while [[ "$#" > 0 ]]; do case $1 in
   --package-only) PACKAGE_ONLY=true;shift;;
   --github-packages) GITHUB_PACKAGES_DEF="$2";shift;shift;;
   --features) FEATURES="$2";shift;shift;;
+  --targets) TARGETS="$2";shift;shift;;
   *) usage "Unknown parameter passed: $1"; shift; shift;;
 esac; done
 
@@ -84,65 +93,87 @@ bail() {
 # The PR linked above uses different folder names, but these ones accurately
 # describe why we need different builds in different environments.
 
+TARGETS_ALL="$TARGETS"
+if [ -z "$TARGETS_ALL" ]; then
+  TARGETS_ALL="nodejs,browser,web,no-modules,zotero"
+fi
+
 if [ -z "$PACKAGE_ONLY" ]; then
-  mkdir -p pkg-scratch
+  mkdir -p $DIR/pkg-scratch
   rm -rf $DEST
   mkdir -p $DEST
 
   ## e.g. build into pkg-scratch/target; move files to $DEST/_target/
   target() {
-    TARGET=$1
-    OUT=$2
-    EXTRA_FEATURES=$3
+    TARGET=${1:-}
+    OUT=${2:-}
+    EXTRA_FEATURES=${3:-}
+    SCRATCH="$DIR/pkg-scratch/$OUT"
     wasm-pack build --release \
       --out-name citeproc_rs_wasm \
       --scope citeproc-rs \
       --target "$TARGET" \
-      --out-dir pkg-scratch/$OUT \
+      --out-dir "$SCRATCH" \
       -- --features "$FEATURES$EXTRA_FEATURES" \
-      && mkdir -p "$DEST/$OUT" \
-      && cp pkg-scratch/$OUT/citeproc_rs_wasm* "$DEST/$OUT/" \
+      || bail "building target $TARGET -> $OUT --features \"$FEATURES$EXTRA_FEATURES\""
+
+    SNIPPETS="$DIR/pkg-scratch/$OUT/snippets"
+    mkdir -p "$DEST/$OUT" \
+      && ([[ -d "$SNIPPETS" ]] && cp -R "$SNIPPETS" "$DEST/$OUT" || true) \
+      && cp -R  $DIR/pkg-scratch/$OUT/citeproc_rs_wasm* "$DEST/$OUT/" \
+      && cp $DIR/pkg-scratch/$OUT/.gitignore "$DEST/" \
       || bail "building target $TARGET -> $OUT --features \"$FEATURES$EXTRA_FEATURES\""
   }
 
-  target nodejs _cjs
-  target browser _esm
-  target web _web
-  target no-modules _no_modules ,no-modules
-  target no-modules _zotero ,zotero
+  echo "$TARGETS_ALL" | sed -n 1'p' | tr ',' '\n' | while read TARGET; do
+      echo TARGET: $TARGET
+      if [ "$TARGET" == "nodejs" ]; then target nodejs _cjs; fi
+      if [ "$TARGET" == "browser" ]; then target browser _esm; fi
+      if [ "$TARGET" == "web" ]; then target web _web; fi
+      if [ "$TARGET" == "no-modules" ]; then target no-modules _no_modules ,no-modules; fi
+      if [ "$TARGET" == "zotero" ]; then target no-modules _zotero ,zotero; fi
+      echo TARGET DONE: $TARGET
+  done
+
 else
   mkdir -p $DEST/_cjs $DEST/_esm $DEST/_web $DEST/_no_modules $DEST/_zotero
 fi
 
-ZOTERO_BINDGEN_SRC="pkg-scratch/_zotero/citeproc_rs_wasm.js"
-ZOTERO_BINDGEN="$DEST/_zotero/citeproc_rs_wasm.js"
-sed -e 's/CITEPROC_RS_ZOTERO_GLOBAL/Zotero.CiteprocRs/g' \
-  < $ZOTERO_BINDGEN_SRC \
-  > "$ZOTERO_BINDGEN" \
-  || bail "could not replace CITEPROC_RS_ZOTERO_GLOBAL"
-cat <<EOF >> "$ZOTERO_BINDGEN"
+if [[ $TARGETS_ALL =~ "zotero" ]]; then
+  ZOTERO_BINDGEN_SRC="$DIR/pkg-scratch/_zotero/citeproc_rs_wasm.js"
+  ZOTERO_BINDGEN="$DEST/_zotero/citeproc_rs_wasm.js"
+  sed -e 's/CITEPROC_RS_ZOTERO_GLOBAL/Zotero.CiteprocRs/g' \
+    < $ZOTERO_BINDGEN_SRC \
+    > "$ZOTERO_BINDGEN" \
+    || bail "could not replace CITEPROC_RS_ZOTERO_GLOBAL"
+  cat <<EOF >> "$ZOTERO_BINDGEN"
 module.exports = wasm_bindgen;
 if (typeof Zotero !== "undefined" && typeof Zotero.CiteprocRs !== "undefined") {
   Object.assign(Zotero.CiteprocRs, wasm_bindgen)
 }
 EOF
+fi
 
-NODEJS_INCLUDE="$DEST/_cjs/citeproc_rs_wasm_include.js"
-NOMOD_INCLUDE="$DEST/_no_modules/citeproc_rs_wasm_include.js"
-ZOTERO_INCLUDE="$DEST/_zotero/citeproc_rs_wasm_include.js"
-sed -e 's/export class/class/' < src/js/include.js \
+NODEJS_INCLUDE=/dev/null
+NOMOD_INCLUDE=/dev/null
+ZOTERO_INCLUDE=/dev/null
+if [[ "$TARGETS_ALL" == *"nodejs"* ]]; then NODEJS_INCLUDE=($DEST/_cjs/snippets/**/include.js); fi
+if [[ "$TARGETS_ALL" == *"no-modules"* ]]; then NOMOD_INCLUDE="$DEST/_no_modules/citeproc_rs_wasm_include.js"; fi
+if [[ "$TARGETS_ALL" == *"zotero"* ]]; then ZOTERO_INCLUDE="$DEST/_zotero/citeproc_rs_wasm_include.js"; fi
+
+sed -e 's/export class/class/' < $DIR/src/js/include.js \
   | tee "$ZOTERO_INCLUDE" \
   | tee "$NODEJS_INCLUDE" \
   > "$NOMOD_INCLUDE" \
   || bail "could not write include.js for no-modules targets"
 # We want the commonjs module.exports in both of these
-cat src/js/commonjs_export.js >> "$NOMOD_INCLUDE" || bail "failed writing $NOMOD_INCLUDE"
-cat src/js/commonjs_export.js >> "$NODEJS_INCLUDE" || bail "failed writing $NODEJS_INCLUDE"
+cat $DIR/src/js/commonjs_export.js >> "$NOMOD_INCLUDE" || bail "failed writing $NOMOD_INCLUDE"
+cat $DIR/src/js/commonjs_export.js >> "$NODEJS_INCLUDE" || bail "failed writing $NODEJS_INCLUDE"
 # zotero is weird
-cat src/js/zotero.js >> "$ZOTERO_INCLUDE" || bail "failed writing $ZOTERO_INCLUDE"
+cat $DIR/src/js/zotero.js >> "$ZOTERO_INCLUDE" || bail "failed writing $ZOTERO_INCLUDE"
 
-cp README.md $DEST/ || bail "could not copy README"
-# cp scripts/model-package.json $DEST/package.json
+cp $DIR/README.md $DEST/ || bail "could not copy README"
+# cp $DIR/scripts/model-package.json $DEST/package.json
 
 TARGET_VERSION=${SET_VERSION:-$CARGO_VERSION}
 
@@ -183,7 +214,7 @@ fi
 
 printf "Writing package.json with: $JQ_ARGS\n$JQ_FILTERS\n"
 
-jq $JQ_ARGS "$JQ_FILTERS" < scripts/model-package.json > $DEST/package.json \
+jq $JQ_ARGS "$JQ_FILTERS" < $DIR/scripts/model-package.json > $DEST/package.json \
   || bail "writing $DEST/package.json using scripts/model-package.json and jq"
 
 # cat $DEST/package.json | jq
