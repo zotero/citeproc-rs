37837b4dc8d75b03234a22e52335e1c63e8aa5d5
Cormac Relf <web@cormacrelf.net>
2021-04-21 17:30:10 +1000

    run end-to-end tests in Firefox ESR 60.9 in CI

--- crates/wasm/js-tests/browser/browser.test.ts
+++ crates/wasm/js-tests/browser/browser.test.ts
@@ -1,18 +1,24 @@
-
-
 // selenium-webdriver tests
+
+if (process.env.MUST_RUN_FIREFOX_TESTS == "1" && !global.browser) {
+    throw new Error(`cannot run firefox tests, probably because no FIREFOX_BINARY_PATH provided`)
+}
+
+var suite = describe.skip;
 if (global.browser) {
-    test("runs in firefox esr 60.9", async () => {
-        jest.setTimeout(30000);
-        const fs = require('fs');
+    suite = describe;
+}
+suite("Integration tests for Firefox ESR 60.9", () => {
+    test("no-modules build works end to end", async () => {
+        // firefox could be first-booting itself. Give it time.
+        jest.setTimeout(60000);
         await browser.get("file://" + __dirname + '/index.html');
         let fail = browser.wait(until.elementLocated(By.id('failure')), 10000);
         let succ = browser.wait(until.elementLocated(By.id('success')), 10000);
-        let el = await Promise.race([fail, succ]);
+        let el = await Promise.any([fail, succ]);
         let id = await el.getAttribute("id");
         let text = await el.getText();
         expect(text).toBe("success");
         expect(id).toBe("success");
-        // throw new Error(para)
     })
-}
+})
--- crates/wasm/js-tests/browser/script.js
+++ crates/wasm/js-tests/browser/script.js
@@ -1,17 +1,3 @@
-
-// Use ES module import syntax to import functionality from the module
-// that we have compiled.
-//
-// Note that the `default` import is an initialization function which
-// will "boot" the module and make it ready to use. Currently browsers
-// don't support natively imported WebAssembly as an ES module, but
-// eventually the manual initialization won't be required!
-//
-// Specifically for citeproc-rs, note that we load the JS file from the
-// _web directory. The other directories are for different environments
-// like Node and WebPack. You could also load from cdnjs or similar with a
-// version number and the /_web/citeproc_rs_wasm.js file path.
-console.log("first");
 const { Driver } = wasm_bindgen;
 
 const mkStyle = (inner, bibliography) => {
@@ -44,36 +30,8 @@ class Fetcher {
 }
 
 async function run() {
-    // First up we need to actually load the wasm file, so we use the
-    // default export to inform it where the wasm file is located on the
-    // server, and then we wait on the returned promise to wait for the
-    // wasm to be loaded.
-    //
-    // It may look like this: `await init('./pkg/without_a_bundler_bg.wasm');`,
-    // but there is also a handy default inside `init` function, which uses
-    // `import.meta` to locate the wasm file relatively to js file.
-    //
-    // Note that instead of a string you can also pass in any of the
-    // following things:
-    //
-    // * `WebAssembly.Module`
-    //
-    // * `ArrayBuffer`
-    //
-    // * `Response`
-    //
-    // * `Promise` which returns any of the above, e.g. `fetch("./path/to/wasm")`
-    //
-    // This gives you complete control over how the module is loaded
-    // and compiled.
-    //
-    // Also note that the promise, when resolved, yields the wasm module's
-    // exports which is the same as importing the `*_bg` module in other
-    // modes
-    await wasm_bindgen('../../pkg-nomod/_no_modules/citeproc_rs_wasm_bg.wasm');
+    await wasm_bindgen('./pkg-nomod/_no_modules/citeproc_rs_wasm_bg.wasm');
 
-    // And afterwards we can use all the functionality defined in wasm.
-    // const result = Driver.new("<>noparse", {}, 2);
     const fetcher = new Fetcher();
     const driver = Driver.new({
         style,
@@ -88,7 +46,6 @@ async function run() {
     await driver.fetchLocales();
     let result = driver.builtCluster("one").unwrap();
     console.log("Built a cite cluster:", result);
-    return;
 }
 
 run()
