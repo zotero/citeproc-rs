5c03cd7cb587f1ee5663a01c8a12dc087703b180
Cormac Relf <web@cormacrelf.net>
2021-04-22 17:43:29 +1000

    js-tests: don't use bleeding edge Promise.any to test!

--- crates/wasm/js-tests/browser/browser.test.ts
+++ crates/wasm/js-tests/browser/browser.test.ts
@@ -15,7 +15,7 @@ suite("Integration tests for Firefox ESR 60.9", () => {
         await browser.get("file://" + __dirname + '/index.html');
         let fail = browser.wait(until.elementLocated(By.id('failure')), 10000);
         let succ = browser.wait(until.elementLocated(By.id('success')), 10000);
-        let el = await Promise.any([fail, succ]);
+        let el = await Promise.race([fail, succ]);
         let id = await el.getAttribute("id");
         let text = await el.getText();
         expect(text).toBe("success");
