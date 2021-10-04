5c0e28bf47bee8a3a4d9b67e0db27c0f264bde91
Cormac Relf <web@cormacrelf.net>
2021-10-05 05:54:06 +1100

    don't duplicate month/season/day checking for date-parts

--- crates/io/src/date/parts.rs
+++ crates/io/src/date/parts.rs
@@ -6,16 +6,23 @@ impl DateOrRange {
     /// From a `"date-parts": [[1999, 12, 31]]`-style value in CSL-JSON. Converts to ISO using the
     /// [chronology::historical::Canon] timeline.
     pub fn from_csl_date_parts_arrays(parts: &[&[i32]]) -> Option<Self> {
+        fn to_tuple(parts: &[i32]) -> Option<(i32, u32, u32)> {
+            let m = *parts.get(1).unwrap_or(&0);
+            let d = *parts.get(2).unwrap_or(&0);
+            let year = *parts.get(0)?;
+            if m >= 0 && d >= 0 {
+                Some((year, m as u32, d as u32))
+            } else {
+                None
+            }
+        }
         Some(match parts {
-            [single] => Edtf::Date(date_from_parts_array(single)?),
+            [single] => Self::from_csl_date_parts(to_tuple(single)?, None)?,
             // it's fine if people want to tack 2 million extra date-parts arrays on the end
             // go ahead, make my day
-            [from, to, ..] => {
-                Edtf::Interval(date_from_parts_array(from)?, date_from_parts_array(to)?)
-            }
+            [from, to] => Self::from_csl_date_parts(to_tuple(from)?, Some(to_tuple(to)?))?,
             _ => return None,
         })
-        .map(DateOrRange::Edtf)
     }
 
     pub(crate) fn from_csl_date_parts(
@@ -49,24 +56,6 @@ fn date_from_csl_json_parts(parts: (i32, u32, u32)) -> Option<Date> {
         m + 4
     } else if (21..=24).contains(&m) {
         m
-    } else {
-        0
-    };
-    let day = if d >= 1 && d <= 31 { d as u32 } else { 0 };
-
-    calendar::date_from_csl_json_parts_ymd(year, month, day)
-}
-
-fn date_from_parts_array(parts: &[i32]) -> Option<Date> {
-    let m = *parts.get(1).unwrap_or(&0);
-    let d = *parts.get(2).unwrap_or(&0);
-    let year = *parts.get(0)?;
-    let month = if (1..=12).contains(&m) {
-        m as u32
-    } else if (13..=16).contains(&m) {
-        m as u32 + 8
-    } else if (21..=24).contains(&m) {
-        m as u32
     } else {
         0
     };
